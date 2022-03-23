18fc5875d2c98b354a64d2a66458e2639e3c858f
Josué Dall'Agnese <josofd@gmail.com>
2022-03-23 15:15:01 -0300

    First commit

--- day-3-processors/apache_spark/pr-movies-analysis/settings.py
+++
@@ -1,6 +0,0 @@
-# get [env] information
-BOOTSTRAP_SERVERS = '143.244.200.162:9094'
-INPUT_USERS_TOPIC = 'src-app-users-json'
-INPUT_MOVIES_TITLES_TOPIC = 'src-app-movies-titles-json'
-OUTPUT_TOPIC_COUNTS_PER_COUNTRY_BATCH = 'output-pyspark-counts-country-batch-json'
-OUTPUT_TOPIC_COUNTS_PER_GENRES_STREAM = 'output-pyspark-counts-genres-stream-json'
